4862dcbcf01f2b44820ee38e03ad9bdc58071ad9
Ruslan Fadeev <github@kinrany.dev>
2025-01-10 23:42:18 +0400

    add the trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,76 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+
+/// A trait for providing examples of a type.
+pub trait Exemplars: Sized {
+    /// Iterate over all available examples.
+    ///
+    /// Must return at least one value.
+    fn exemplars() -> impl IntoIterator<Item = Self>;
+    /// Returns the primary example value.
+    ///
+    /// The value is the same as the first value returned from `exemplars()`.
+    fn exemplar() -> Self {
+        Self::exemplars()
+            .into_iter()
+            .next()
+            .expect("invalid impl Exemplars: must provide at least one example value")
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Exemplars for () {
+    fn exemplars() -> impl IntoIterator<Item = Self> {
+        [()]
     }
+}
+
+/// Implement `Exemplars` for number types with 1 as the primary example.
+///
+/// Generates impls like this one:
+///
+/// ```ignore
+/// impl Exemplars for usize {
+///     fn exemplars() -> impl IntoIterator<Item = Self> {
+///         1..=Self::MAX
+///     }
+/// }
+/// ```
+macro_rules! impl_for_number_types {
+    ($($t:ident),+) => {$(
+        impl Exemplars for $t {
+            fn exemplars() -> impl IntoIterator<Item = Self> {
+                1..=Self::MAX
+            }
+        }
+    )+}
+}
+impl_for_number_types!(usize, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128);
+
+impl<T: Exemplars> Exemplars for Option<T> {
+    fn exemplars() -> impl IntoIterator<Item = Self> {
+        T::exemplars().into_iter().map(Some).chain([None])
+    }
+}
+
+#[cfg(feature = "alloc")]
+mod alloc {
+    extern crate alloc;
+
+    use super::Exemplars;
+
+    impl Exemplars for alloc::string::String {
+        fn exemplars() -> impl IntoIterator<Item = Self> {
+            ["example".into()]
+        }
+    }
+
+    impl<T: Exemplars> Exemplars for alloc::vec::Vec<T> {
+        fn exemplars() -> impl IntoIterator<Item = Self> {
+            T::exemplars().into_iter().map(|x| alloc::vec![x])
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+mod std {
+    extern crate std;
 }
